bbfbf6e3026442b583c6728fd7d829a7c606c3f3
howdym <32106354+howdym@users.noreply.github.com>
2019-05-21 14:02:25 -0500

    first push

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import cleanup
 import change_detection
 
 """Authentication"""
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets'
-    ,"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets'
+    , "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 
@@ -17,6 +17,7 @@ TrackingHistory = SpreadSheet.worksheet("Inventory Tracking History")
 RemovalHistory = SpreadSheet.worksheet("Inventory Removal History")
 AdditionHistory = SpreadSheet.worksheet("Inventory Addition History")
 MovingHistory = SpreadSheet.worksheet("Moving Locations History")
+
 
 def CleanAll():
     print("Starting Cleanup")
@@ -33,16 +34,18 @@ def CleanAll():
 
 
 OldAdditionData = AdditionHistory.get_all_records()
+OldRemovalData = RemovalHistory.get_all_records()
 
 cleanup_counter = 240
 print("Sheet Scraping Cycle Beginning")
 while True:
-    # check every 100 seconds, and hopefully Google Sheets API doesn't scream
-    time.sleep(100)
-    AdditionData = AdditionHistory.get_all_records()
-    changed = change_detection.changed_rows(OldAdditionData, AdditionData)
-    OldAdditionData = AdditionData
+    # check every 30 seconds, and hopefully Google Sheets API doesn't scream
+    time.sleep(30)
     print("Now we check!")
+
+    # Handle Addition
+    # find the changed rows
+    changed = change_detection.changed_rows(OldAdditionData, AdditionHistory.get_all_records())
 
     # If a history has been cleared, don't do anything
     if AdditionHistory.row_count < 3:
@@ -54,9 +57,30 @@ while True:
     else:
         print("No addition requests submitted")
 
+        # This is for removal
+
+    # Handle Removal
+    # find the removed rows
+    changed_removal = change_detection.changed_rows(OldRemovalData, RemovalHistory.get_all_records())
+
+    if RemovalHistory.row_count < 3:
+        print("Removal Sheet is Empty. Maybe cause it got recently cleared :(")
+    elif changed_removal:  # if rows are changed, something has been added
+        for row in changed_removal:
+            new_removal.new_removal(RemovalHistory, InventorySheet, row)
+            print("Items removed from inventory!")
+    else:
+        print("No addition requests submitted")
+
     # every 2 hours wipe the empty rows out
     if cleanup_counter == 0:
         CleanAll()
         cleanup_counter = 240
     else:
         cleanup_counter = cleanup_counter - 1
+
+    # Load old data for a later comparison
+    OldAdditionData = AdditionHistory.get_all_records()
+    AdditionHistory = SpreadSheet.worksheet("Inventory Addition History")
+    OldRemovalData = RemovalHistory.get_all_records()
+    RemovalHistory = SpreadSheet.worksheet("Inventory Removal History")
